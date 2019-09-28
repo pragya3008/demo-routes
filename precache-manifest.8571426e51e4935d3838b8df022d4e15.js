797acf3a482ea57e154451d17d971d45b1b5d09a
pragya3008 <pragya.neural@gmail.com>
2019-09-28 17:13:39 +0530

    Updates

---
+++ precache-manifest.8571426e51e4935d3838b8df022d4e15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39355e8f76ca4c3d953a00a95608ac17",
+    "url": "/demo-routes/index.html"
+  },
+  {
+    "revision": "9437a4cf1fd7a645f0ad",
+    "url": "/demo-routes/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "167cc2e93eab124ad316",
+    "url": "/demo-routes/static/js/2.38f6bc54.chunk.js"
+  },
+  {
+    "revision": "9437a4cf1fd7a645f0ad",
+    "url": "/demo-routes/static/js/main.7ddb764e.chunk.js"
+  },
+  {
+    "revision": "189c7d58dd4f839972f3",
+    "url": "/demo-routes/static/js/runtime-main.74216ba4.js"
+  }
+]);
